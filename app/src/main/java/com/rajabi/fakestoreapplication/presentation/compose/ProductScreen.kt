d5341a0db767b4defd1f7804242cfc049f366bb9
behnazrp68 <behnaz.rajabi.p@gmail.com>
2024-01-14 12:17:02 +0330

    searchBar is added

--- app/src/main/java/com/rajabi/fakestoreapplication/presentation/compose/ProductScreen.kt
+++ app/src/main/java/com/rajabi/fakestoreapplication/presentation/compose/ProductScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -27,10 +29,11 @@ import com.rajabi.fakestoreapplication.presentation.viewmodel.FakeStoreViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProductScreen(onNavigateToAdvertisingScreen: (Int) -> Unit,
-                  factory: FakeStoreViewModelFactory,
-                  modifier: Modifier = Modifier,
-                  fakeStoreViewModel: FakeStoreViewModel = viewModel(factory = factory)
+fun ProductScreen(
+    onNavigateToAdvertisingScreen: (Int) -> Unit,
+    factory: FakeStoreViewModelFactory,
+    modifier: Modifier = Modifier,
+    fakeStoreViewModel: FakeStoreViewModel = viewModel(factory = factory)
 ) {
     LaunchedEffect(key1 = true) {
         fakeStoreViewModel.getProducts()
@@ -38,8 +41,46 @@ fun ProductScreen(onNavigateToAdvertisingScreen: (Int) -> Unit,
     }
 
     val productList = fakeStoreViewModel.products.observeAsState()
-    Column (horizontalAlignment = Alignment.End,
-        modifier = Modifier.fillMaxWidth()){
+    //Collecting states from ViewModel
+    val searchText by fakeStoreViewModel.searchText.collectAsState()
+    val isSearching by fakeStoreViewModel.isSearching.collectAsState()
+    val countriesList by fakeStoreViewModel.searchList.collectAsState()
+    Column(
+        horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxWidth()
+    ) {
+        SearchBar(
+            query = searchText,//text showed on SearchBar
+            onQueryChange = fakeStoreViewModel::onSearchTextChange, //update the value of searchText
+            onSearch = fakeStoreViewModel::onSearchTextChange, //the callback to be invoked when the input service triggers the ImeAction.Search action
+            active = isSearching, //whether the user is searching or not
+            onActiveChange = { fakeStoreViewModel.onToogleSearch() }, //the callback to be invoked when this search bar's active state is changed
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(), contentPadding = PaddingValues(16.dp)
+            ) {
+                productList.value?.data?.filter {
+                    it.title.uppercase().contains(searchText.trim().uppercase())
+                }.apply {
+                    var products = this
+
+                    products?.let { product ->
+                        product?.size?.let { size ->
+                            items(size) { item ->
+                                ProductItem(product.get(item), modifier = Modifier.clickable {
+
+                                })
+
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        }
 
         LazyColumn(
             modifier = Modifier.fillMaxWidth(),
@@ -51,7 +92,7 @@ fun ProductScreen(onNavigateToAdvertisingScreen: (Int) -> Unit,
                 var products =
                     this.value?.data
 
-                products?.let {product ->
+                products?.let { product ->
                     product?.size?.let { size ->
                         items(size) { item ->
                             ProductItem(
@@ -65,4 +106,5 @@ fun ProductScreen(onNavigateToAdvertisingScreen: (Int) -> Unit,
                 }
             }
         }
-    }}
+    }
+}
--- app/src/main/java/com/rajabi/fakestoreapplication/presentation/viewmodel/FakeStoreViewModel.kt
+++ app/src/main/java/com/rajabi/fakestoreapplication/presentation/viewmodel/FakeStoreViewModel.kt
@@ -72,5 +72,39 @@ class FakeStoreViewModel
         return false
     }
 
+//    //first state whether the search is happening or not
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
 
+    //second state the text typed by the user
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    //third state the list to be filtered
+    private val _searchList = MutableStateFlow(products.value?.data)
+    val searchList = searchText
+        .combine(_searchList)
+        { text, products ->//combine searchText with _contriesList
+            if (text.isBlank()) { //return the entery list of countries if not is typed
+                products
+            }
+            products?.filter { products ->// filter and return a list of countries based on the text the user typed
+                products.title.uppercase().contains(text.trim().uppercase())
+            }
+        }.stateIn(//basically convert the Flow returned from combine operator to StateFlow
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),//it will allow the StateFlow survive 5 seconds before it been canceled
+            initialValue = products.value
+        )
+
+    fun onSearchTextChange(text: String) {
+        _searchText.value = text
+    }
+
+    fun onToogleSearch() {
+        _isSearching.value = !_isSearching.value
+        if (!_isSearching.value) {
+            onSearchTextChange("")
+        }
+    }
 }
